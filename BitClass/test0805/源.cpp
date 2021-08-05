004bd1ca3f37d86d02f6d3dcb833276d36c47765
810760487@qq.com <810760487@qq.com>
2021-08-05 12:45:35 +0800

    20210805

---
+++ BitClass/test0805/源.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//int main() {
+//    int sum = 0;
+//    char str[10] = { 0 };
+//    gets_s(str);
+//    char* p = str;
+//    while (*p) {
+//        int temp = *p - 48;
+//        if (temp % 2 == 0)
+//            temp = 0;
+//        else
+//            temp = 1;
+//        sum = sum*10+ temp;
+//        p++;
+//    }
+//    printf("%d", sum);
+//    return 0;
+//}
