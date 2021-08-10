4d39925d2f8a48c72ee922a6cdeb325ef639879c
810760487@qq.com <810760487@qq.com>
2021-08-10 10:07:02 +0800

    代码

---
+++ BitClass/test0810/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
+
+//int main() {
+//	char ch = 'w';
+//	char* p1 = &ch;
+//	char* p2 = "hello word";//将常量字符串的第一个值地址赋给指针
+//	printf("%c\n", *(p2 + 1));
+//	printf("%s\n", p2);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char str1[] = "hello bit.";
+//	char str2[] = "hello bit.";
+//	char* str3 = "hello bit.";
+//	char* str4 = "hello bit.";
+//	if (str1 == str2)
+//		printf("str1 and str2 are same\n");
+//	else
+//		printf("str1 and str2 are not same\n");
+//	if (str3 == str4)
+//		printf("str3 and str4 are same\n");
+//	else
+//		printf("str3 and str4 are not same\n");
+//	return 0;
+//}
+
+
+//int main() {
+//	char* arr[20];
+//	int* arr2[20];
+//	return 0;
+//}
+
+
