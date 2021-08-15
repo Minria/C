d031e758eb4ae4f9730da76e56a2d76ead648c02
810760487@qq.com <810760487@qq.com>
2021-08-15 14:20:40 +0800

    update20210815

---
+++ BitClass/test0815/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+//int main() {
+//	int a = 4, b = 3;
+//	int* q = &a;
+//	int* w = &b;
+//	int* e = w;
+//	w = NULL;
+//	return 0;
+//}
+
+//int fun(int x) {
+//	int count = 0;
+//	while (x) {
+//		count++;
+//		x = x & (x - 1);
+//	}
+//	return count;
+//}
+//int main() {
+//	printf("%d", fun(2019));
+//	return 0;
+//}
+
+//unsigned long g_ulGlobal = 0;
+//void GlobalInit(unsigned long ulArg) {
+//	ulArg = 0x01;
+//	return;
+//}
+//void test() {
+//	GlobalInit(g_ulGlobal);
+//	printf("%lu", g_ulGlobal);
+//	return;
+//}
+//int main() {
+//	test();
+//	return 0;
+//}
