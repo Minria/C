d4f9361139a517ab6187bc63b53081b2af107458
810760487@qq.com <810760487@qq.com>
2021-08-19 08:48:43 +0800

    0000

--- BitClass/test0818/test.c
+++ BitClass/test0818/test.c
@@ -123,10 +123,42 @@
 //		}
 //	}
 //}
-//int main() {
-//	int nums[4][4] = { {1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15} };
-//	int ret = find(nums, 0, 0, 3, 3, );
-//	printf("%d", ret);
-//	return 0;
-//
+//bool Find(int* matrix, int m, int n, int key)
+//{
+//	if (matrix == NULL || m < 1 || n < 1)
+//		return false;
+//	int row = 0;
+//	int col = n - 1;
+//	while (row <= m - 1 && col >= 0)
+//	{
+//		if (matrix[row * n + col] == key)
+//			return true;
+//		else if (matrix[row * n + col] > key)
+//			col--;
+//		else
+//			row++;
+//	}
+//	return false;
 //}
+//int find(int* nums, int m, int n, int key){//m行n列，找到返回1，否则返回0
+//	if (nums == NULL || m < 1 || n < 1)
+//		return 0;
+//	int row = 0;
+//	int col = n - 1;
+//	while (row <= m - 1 && col >= 0){
+//		if (nums[row * n + col] == key)
+//			return 1;
+//		else if (nums[row * n + col] > key)
+//			col--;
+//		else
+//			row++;
+//	}
+//	return 0;
+//}
+int main() {
+	int nums[4][4] = { {1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15} };
+	int ret = find(nums,4,4,8);
+	printf("%d", ret);
+	return 0;
+
+}
