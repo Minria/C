2f9d25ab5b62aabcef8e766cadc533570e63efe1
810760487@qq.com <810760487@qq.com>
2021-08-20 21:05:59 +0800

    0

--- BitClass/test0820/test.c
+++ BitClass/test0820/test.c
@@ -117,3 +117,16 @@
 //	printf("%d", a);
 //	return 0;
 //}
+
+int main()
+{
+	char a[] = "123456789", * p = a;
+	int i = 0;
+	while (*p)
+	{
+		if (i % 2 == 0) *p = '!';
+		i++; p++;
+	}
+	puts(a);
+	return 0;
+}
