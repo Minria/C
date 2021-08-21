165ddce1d969bc13e3988004d8b66d2ec58d50fb
810760487@qq.com <810760487@qq.com>
2021-08-21 08:39:34 +0800

    0

---
+++ BitClass/test0821/test.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+
+//#define N 3
+//#define Y(n) ((N+1)*n)
+//int main() {
+//	int z =2* (N * Y(5 + 1));
+//	printf("%d", z);
+//	return 0;
+//}
+
+//int main() {
+//	printf("%f", (double)(10 / 4 * 4));
+//	return 0;
+//}
+
+
+//int main() {
+//	int i, x, y;
+//	i = x = y = 0;
+//	do {
+//		++i;
+//		if (i % 2)
+//			x += i;
+//		i++;
+//		y += i++;
+//	} while (i <= 7);
+//	printf("%d %d %d", i, x, y);
+//	return 0;
+//}
+//
+//int main() {
+//	int j = 4, i = j;
+//	for (; i <= 2 * j; i++) {
+//		switch (i/j)
+//		{
+//		case 0:
+//		case 1:printf("*"); break;
+//		case 2:printf("#"); break;
+//		}
+//	}
+//	return 0;
+//}
+//int main() {
+//	int x = 1, y = 012;
+//	printf("%d", y);
+//	return 0;
+//}
+
+//int main() {
+//	int arr[] = { 1,2,3,4,5 };
+//	int* ptr = (int*)(&arr + 1);
+//	printf("%d %d", *(arr + 1), *(ptr - 1));
+//	return 0;
+//}
+
+//int main() {
+//	unsigned short a = 0;
+//	--a;
+//	printf("%d", a);
+//	return 0;
+//}
