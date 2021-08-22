fcbc994d2c5bfd136d771ebf8f480116d436c4c0
810760487@qq.com <810760487@qq.com>
2021-08-22 12:29:04 +0800

    000

--- BitClass/test0822/test.c
+++ BitClass/test0822/test.c
@@ -94,3 +94,42 @@
 //	printf("%d %d %d", arr[0], arr[1], arr[2]);
 //	return 0;
 //}
+
+//¶þ·Ö·¨
+//int Max3(int A, int B, int C) {
+//	return (A > B) ? (A > C ? A : C) : (B > C ? B : C);
+//}
+//int DivideAndConquer(int List[], int left, int right) {
+//	int MaxLeftSum, MaxRightSum;    
+//	int MaxLeftBorderSum, MaxRightBorderSum;   
+//	int LeftBorderSum, RightBorderSum;
+//	int center, i;
+//
+//	if (left == right) {
+//		if (List[left] > 0)	return List[left];
+//		else return 0;
+//	}
+//	center = (left + right) / 2;   
+//	MaxLeftSum = DivideAndConquer(List, left, center);    
+//	MaxRightSum = DivideAndConquer(List, center + 1, right);   
+//
+//	MaxLeftBorderSum = 0;	LeftBorderSum = 0;
+//	for (i = center; i >= left; i--) {
+//		LeftBorderSum += List[i];
+//		if (LeftBorderSum > MaxLeftBorderSum)
+//			MaxLeftBorderSum = LeftBorderSum;
+//	}
+//
+//	MaxRightBorderSum = 0;	RightBorderSum = 0;
+//	for (i = center + 1; i <= right; i++) {
+//		RightBorderSum += List[i];
+//		if (RightBorderSum > MaxRightBorderSum)
+//			MaxRightBorderSum = RightBorderSum;
+//	}
+//	return Max3(MaxLeftSum, MaxRightSum, MaxLeftBorderSum + MaxRightBorderSum);
+//}
+//
+//int MaxSubseqSum3(int List[], int N) {
+//	return DivideAndConquer(List, 0, N - 1);
+//}
+
