f9875fe8850904cd202e1fef01ab5f430ef3ee43
810760487@qq.com <810760487@qq.com>
2021-08-31 22:12:04 +0800

    修正

--- BitClass/test0831/test.c
+++ BitClass/test0831/test.c
@@ -34,3 +34,36 @@
 //	bubbleSort(arr, 5);
 //	return 0;
 //}
+
+
+//int main() {
+//	int a = 9;
+//	float b = 10.0;
+//	void* p1 = &a;
+//	void* p2 = &b;
+//	return 0;
+//}
+
+//int main() {
+//	int arr[] = { 1,2,3,4 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr[0]));
+//	printf("%d\n", sizeof(&arr));
+//	printf("%d\n", sizeof(&arr + 1));
+//	printf("%d\n", sizeof(*arr));
+//	printf("%d\n", sizeof(*arr + 1));
+//}
+
+//int main() {
+//	int a[] = { 1,2,3,4 };
+//	printf("%d\n", sizeof(a));//16
+//	printf("%d\n", sizeof(a + 0));//4/8
+//	printf("%d\n", sizeof(*a));//4
+//	printf("%d\n", sizeof(a + 1));//4/8
+//	printf("%d\n", sizeof(a[1]));//4
+//	printf("%d\n", sizeof(&a));//16
+//	printf("%d\n", sizeof(*&a));//16
+//	printf("%d\n", sizeof(&a + 1));//4
+//	printf("%d\n", sizeof(&a[0]));//4/8
+//	printf("%d\n", sizeof(&a[0] + 1));//4/8
+//}
