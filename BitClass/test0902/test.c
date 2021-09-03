0bcc8b6cf0aa7e90db029bdd2a61288f5bff8139
810760487@qq.com <810760487@qq.com>
2021-09-03 10:05:41 +0800

    提交

--- BitClass/test0902/test.c
+++ BitClass/test0902/test.c
@@ -2,3 +2,27 @@
 #include<stdio.h>
 #include<string.h>
 #include<malloc.h>
+
+
+//int main() {
+//	int a[] = { 1,2,3,4 };
+//	printf("%d\n", sizeof(a));//16
+//	printf("%d\n", sizeof(a + 0));//4/8
+//	printf("%d\n", sizeof(*a));//4
+//	printf("%d\n", sizeof(a + 1));//4/8
+//	printf("%d\n", sizeof(a[1]));//4
+//	printf("%d\n", sizeof(&a));//4/8
+//	printf("%d\n", sizeof(*&a));//4
+//	printf("%d\n", sizeof(&a + 1));//4/8
+//	printf("%d\n", sizeof(&a[0]));//4/8
+//	printf("%d\n", sizeof(&a[0] + 1));//4/8
+//	return 0;
+//}
+
+
+//int main() {
+//	char str[] = "abcdefg";
+//	printf("%p\n", str);
+//	printf("%p\n", &str);
+//	return 0;
+//}
