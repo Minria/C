1a07e67245dfbf378e0be6443821e4683561efa6
wfm1021 <810760487@qq.com>
2021-09-05 14:00:48 +0800

    ？？？？

--- BitClass/test0905/test.c
+++ BitClass/test0905/test.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
-int main() {
-	int a = 1;
-	printf("%d", a);
-	return 0;
-}
+//int main() {
+//	int a = 1;
+//	printf("%d", a);
+//	return 0;
+//}
+
+//p[i]==*(p+i)
+//p[i][j]==*(*(p+i)+j)
+//int main() {
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));//4¡Á12=48
+//	printf("%d\n", sizeof(a[0][0]));//4
+//	printf("%d\n", sizeof(a[0]));//4¡Á4=16
+//	printf("%d\n", sizeof(a[0] + 1));//&a[0][1],4»ò8
+//	printf("%d\n", sizeof(*(a[0] + 1)));//4
+//	printf("%d\n", sizeof(a + 1));//4/8
+//	printf("%d\n", sizeof(*(a + 1)));//16
+//	printf("%d\n", sizeof(&a[0] + 1));//4/8
+//	printf("%d\n", sizeof(*(&a[0] + 1)));//16
+//	printf("%d\n", sizeof(*a));//16
+//	return 0;
+//}
+
