f7c1d119c831a0a1e3efb1076c01ed124a26fae5
wfm1021 <810760487@qq.com>
2021-09-18 18:21:58 +0800

    20210918

---
+++ 库函数模拟实现/memcpy/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+void* my_memcpy(void* str1, const void* str2, size_t n) {
+	while (n--) {
+		*((char*)str1 + n) = *((char*)str2 + n);
+	}
+	return str1;
+}
+int main() {
+	char str1[] = "abcdefg";
+	my_memcpy(str1, str1 + 4, 2);
+	printf("%s", str1);
+	return 0;
+}
