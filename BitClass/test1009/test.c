dda963736b6cca2410bb98b8403ffe4905b03f14
wfm1021 <810760487@qq.com>
2021-10-09 15:59:18 +0800

    000

---
+++ BitClass/test1009/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+//#define ADD(X) (X)*(X)
+//int main() {
+//	int a = 5;
+//	int ret1 = ADD(a);
+//	int ret2 = ADD(a + 1);
+//	printf("%d %d", ret1, ret2);
+//	return 0;
+//}
+
+//#define ADD(X) ((X)+(X))
+//int main() {
+//	int a = 5;
+//	printf("%d", 2 * ADD(a));
+//	return 0;
+//}
+
+//int main() {
+//	char* p = "hello ""world";
+//	printf(p);
+//	return 0;
+//}
+
+
+//#define PRINT(a) printf("the value of "#a" is %d\n", a)
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	PRINT(a);
+//	PRINT(b);
+//	PRINT(c);
+//	return 0;
+//}
+//
+//#define ADDTOSUM(X,Y) X##Y
+//int main() {
+//	printf("%d", ADDTOSUM(1, 2));
+//	return 0;
+//}
