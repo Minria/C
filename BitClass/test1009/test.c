6885b3d49e91fd77df25d515f4c9d758170e0c74
wfm1021 <810760487@qq.com>
2021-10-09 19:44:29 +0800

    del before

--- BitClass/test1009/test.c
+++ BitClass/test1009/test.c
@@ -42,3 +42,28 @@
 //	printf("%d", ADDTOSUM(1, 2));
 //	return 0;
 //}
+
+
+#include <stdio.h>
+
+//
+//int  main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag * 1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+int main() {
+	int a = printf("%d", 432);
+	int b = printf("abcdef");
+	printf("%d %d", a, b);
+	return 0;
+}
