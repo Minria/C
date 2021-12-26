be6304234012dda1542e4391e0d8eb7f9f295c17
Glacier-blue <810760487@qq.com>
2021-12-26 20:31:49 +0800

    常见的七种基于比较排序

--- Sort/head.h
+++ Sort/head.h
@@ -1,3 +1,15 @@
 #include<stdio.h>
-
+#include<stdbool.h>
+#include<malloc.h>
 void insertSort(int* nums, int numsSize);
+void swap(int* nums, int i, int j);
+void halfInsertSort(int* nums, int numsSize);
+void shellSort(int* nums, int numsSize);
+void bubbleSort(int* nums, int numsSize);
+void quickSort(int* nums, int left, int right);
+void selectSort(int* nums, int numsSize);
+void heapSort(int* nums, int numSize);
+void adjustDown(int* nums, int root, int numsSize);
+void mergeAnd(int* nums, int left, int right);
+void mergeInterval(int* nums, int left, int right);
+void mergeSort(int* nums, int numsSize);
--- Sort/main.c
+++ Sort/main.c
@@ -8,8 +8,16 @@ void print(int* nums, int numsSize) {
 	printf("\n");
 }
 int main() {
-	int arr[] = { 2,4,3,5,6,1,3,4 };
-//	insertSort(arr, 8);
-	print(arr, 8);
+	int nums[] = { 16,2,3,14,15,6,7,11,9,0,10,8,13,12,4,5,1};
+	int numsSize = sizeof(nums) / 4;
+	//insertSort(nums, numsSize);
+	//halfInsertSort(nums, numsSize);
+	//shellSort(nums, numsSize);
+	//bubbleSort(nums, numsSize);
+	//quickSort(nums, 0, numsSize - 1);
+	//selectSort(nums, numsSize);
+	//heapSort(nums, numsSize);
+	mergeSort(nums, numsSize);
+	print(nums,numsSize);
 	return 0;
 }
