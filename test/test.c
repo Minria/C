c415c4bd3f852522e2e2fa408f6198d025e56b9e
Glacier-blue <810760487@qq.com>
2022-01-23 11:41:14 +0800

    20220123

--- test/test.c
+++ test/test.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 
+
+typedef struct ListNode {
+	int val;
+	struct ListNode* next;
+}ListNode;
 int main() {
-	int arr[10] = { 1,2,3,4,5,6,7,8 };
-	int* p = arr;
-	printf("%d\n", *p++);
-	printf("%d\n", *p);
+	//int arr[10] = { 1,2,3,4,5,6,7,8 };
+	//int* p = arr;
+	//printf("%d\n", *p++);
+	//printf("%d\n", *p);
+	int ret = sizeof(ListNode);
+	printf("%d", ret);
 }
